266cdda2ce97f18c41e5cd781377edc3ae3c3ddc
Nicholas Pizzino <pizzaboy@iprimus.com.au>
2014-05-09 18:00:28 +0800

    IMPORTANT.c
    Please read by the weekend!

---
+++ IMPORTANT.c
@@ -0,0 +1,23 @@
+// Ok Everyone, this is a message from Nick.
+// We have to attack these activities like there is no tommorrow!
+// Here is a list of activities, and a line space under them.
+// Please input your name for AT LEAST 25% of these activities.
+// Multiple people can work on the same activity.
+// All of them are due by Sunday/Monday.
+
+// URGENT: The testGame.c was overdue from last week.
+
+// testGame.c
+// *Already A File On This Repository*
+// Needs checking
+
+
+// Game.c
+// *Already A File On This Repository*
+
+
+// runGame() Function
+// https://www.openlearning.com/courses/99luftballons/Activities/RungameFunction
+
+
+// Please read ASAP!
