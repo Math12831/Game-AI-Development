80ed91d08d372a25f211552f4e2bb43d27ed2529
Nicholas Pizzino <pizzaboy@iprimus.com.au>
2014-05-09 21:42:19 +0800

    Small Update
    Just fixing Grammar and adding a participant.

--- IMPORTANT.c
+++ IMPORTANT.c
@@ -1,7 +1,7 @@
 // Ok Everyone, this is a message from Nick.
 // We have to attack these activities like there is no tommorrow!
 // Here is a list of activities, and a line space under them.
-// Please input your name for AT LEAST 25% of these activities.
+// Please input your name for AT LEAST 1 of these activities.
 // Multiple people can work on the same activity.
 // All of them are due by Sunday/Monday.
 
@@ -11,6 +11,7 @@
 // *Already A File On This Repository*
 // Needs checking
 
+// Currently Nicholas Pizzino Working On It
 
 // Game.c
 // *Already A File On This Repository*
