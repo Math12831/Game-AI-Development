a9953ddcc938145f73083b8bae237e3ce48499fa
jimbo644664 <james.arcus64@gmail.com>
2014-05-09 21:54:40 +0800

    Created runGame.c
    Just copied in from OpenLearning site, will modify and define functions.

---
+++ runGame.c
@@ -0,0 +1,20 @@
+#include "Game.h"
+
+// Given to us by project
+
+// Game g  = newGame(..);
+// Int currentPlayer = 0;
+// While(getKPI(g, currentPlayer) < WIN_KPI) {
+// diceValue = rollDice();
+// throwDice(g, diceValue);
+// action move = bestMove(g);
+// While (move != PASS) {
+// Assert(isLegal(g, move);
+// If(move.actionCode == START_SPINOFF) {
+// diveValue = rollDice();
+// Decide
+// }
+// makeAction(g, move);
+// }
+// currentPlayer = nextPlayer(currentPlayer);
+// }
