b4c63c5eb11cac102d2461f93c41b548d7407c22
James Arcus <james.arcus64@gmail.com>
2014-05-11 17:00:17 +0800

    Almost finished runGame.c
    Just need comments and a bit of touching up!

--- runGame.c
+++ runGame.c
@@ -1,25 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
 #include "Game.h"
-
-// Given to us by project
-
-// Game g  = newGame(..);
-// Int currentPlayer = 0;
-// While(getKPI(g, currentPlayer) < WIN_KPI) {
-// diceValue = rollDice();
-// throwDice(g, diceValue);
-// action move = bestMove(g);
-// While (move != PASS) {
-// Assert(isLegal(g, move);
-// If(move.actionCode == START_SPINOFF) {
-// diveValue = rollDice();
-// Decide
-// }
-// makeAction(g, move);
-// }
-// currentPlayer = nextPlayer(currentPlayer);
-// }
 
 #define WIN_KPI 150
 
@@ -27,13 +11,16 @@ int main (int argc, char *argv[]);
 
 void getArray(int array[], int num);
 int rollDice();
-action getMove(Game g);
+action getMove();
+void printGame(Game g);
 
 int main (int argc, char *argv[]) {
   int discipline[NUM_REGIONS];
   int dice[NUM_REGIONS];
   
+  printf("Enter the discipline codes of the regions: ");
   getArray(discipline, NUM_REGIONS);
+  printf("Enter the dice values of the regions: ");
   getArray(dice, NUM_REGIONS);
   
   Game g = newGame(discipline, dice);
@@ -43,13 +30,14 @@ int main (int argc, char *argv[]) {
     diceValue = rollDice();
     throwDice(g, diceValue);
     currentPlayer = getWhoseTurn(g);
+    printGame(g);
     
     action move = getMove(g);
     while (move.actionCode != PASS){
-      assert(isLegalAction(g, move));
+      assert(isLegalAction(g, move) == TRUE);
       if (move.actionCode == START_SPINOFF) {
         diceValue = rollDice();
-        if (diceValue <= 4 || dicevalue >= 10) { // Gives probability of 1/3
+        if (diceValue <= 4 || diceValue >= 10) { // Gives probability of 1/3
           move.actionCode = OBTAIN_IP_PATENT;
         } else {
           move.actionCode = OBTAIN_PUBLICATION;
@@ -59,6 +47,8 @@ int main (int argc, char *argv[]) {
       move = getMove();
     }
   }
+  
+  return EXIT_SUCCESS;
 }
 
 // Reads in an array of numbers
@@ -76,12 +66,77 @@ void getArray(int array[], int num) {
 
 int rollDice() {
   int random;
+  printf("Enter the dice roll: ");
   scanf("%d", &random);
   return random;
 }
 
-action getMove(Game g) {
+action getMove() {
   action move;
-  // TODO: Write getMove
+  char currentMove[24];
+  
+  int valid = FALSE;
+  while (valid != TRUE) {
+    printf("Enter the action code to perform: ");
+    scanf("%s", currentMove);
+    
+    if (strcmp(currentMove, "PASS") == 0) {
+      move.actionCode = PASS;
+      valid = TRUE;
+      
+    } else if (strcmp(currentMove, "BUILD_CAMPUS") == 0) {
+      move.actionCode = BUILD_CAMPUS;
+      printf("Enter the location in path format: ");
+      scanf("%s", move.destination);
+      valid = TRUE;
+      
+    } else if (strcmp(currentMove, "BUILD_GO8") == 0) {
+      move.actionCode = BUILD_GO8;
+      printf("Enter the location in path format: ");
+      scanf("%s", move.destination);
+      valid = TRUE;
+      
+    } else if (strcmp(currentMove, "OBTAIN_ARC") == 0) {
+      move.actionCode = OBTAIN_ARC;
+      printf("Enter the location in path format: ");
+      scanf("%s", move.destination);
+      valid = TRUE;
+      
+    } else if (strcmp(currentMove, "START_SPINOFF") == 0) {
+      move.actionCode = START_SPINOFF;
+      valid = TRUE;
+      
+    } else if (strcmp(currentMove, "RETRAIN_STUDENTS") == 0) {
+      move.actionCode = RETRAIN_STUDENTS;
+      printf("Enter the discipline number to convert from: ");
+      scanf("%d", &(move.disciplineFrom));
+      printf("Enter the discipline number to convert from: ");
+      scanf("%d", &(move.disciplineTo));
+      valid = TRUE;
+      
+    } else {
+      printf("Action not valid!\n");
+      
+    }
+    
+  }
+  
   return move;
+}
+
+void printGame(Game g) {
+  int currentPlayer = getWhoseTurn(g);
+  printf("It is the turn of player %d\n", currentPlayer);
+  printf("They have %d KPI points\n", getKPIpoints(g, currentPlayer));
+  printf("They have %d ARCs\n", getARCs(g, currentPlayer));
+  printf("They have %d GO8s\n", getGO8s(g, currentPlayer));
+  printf("They have %d campuses\n", getCampuses(g, currentPlayer));
+  printf("They have %d IPs\n", getIPs(g, currentPlayer));
+  printf("They have %d publications\n", getPublications(g, currentPlayer));
+  printf("They have %d ThDs, %d BPSs, %d B?s, %d MJs, %d MTVs and %d M$s",
+          getStudents(g, currentPlayer, STUDENT_THD), getStudents(g, currentPlayer, STUDENT_BPS),
+          getStudents(g, currentPlayer, STUDENT_BQN), getStudents(g, currentPlayer, STUDENT_MJ),
+          getStudents(g, currentPlayer, STUDENT_MTV), getStudents(g, currentPlayer, STUDENT_MMONEY));
+  
+  return;
 }
