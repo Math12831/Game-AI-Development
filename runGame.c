1394f49892da4f6e2a6225562303b4b3d63068f2
James Arcus <james.arcus64@gmail.com>
2014-05-11 14:11:13 +0800

    Update runGame.c
    Wrote runGame and some supporting functions

--- runGame.c
+++ runGame.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "Game.h"
 
 // Given to us by project
@@ -25,6 +26,8 @@
 int main (int argc, char *argv[]);
 
 void getArray(int array[], int num);
+int rollDice();
+action getMove(Game g);
 
 int main (int argc, char *argv[]) {
   int discipline[NUM_REGIONS];
@@ -34,13 +37,34 @@ int main (int argc, char *argv[]) {
   getArray(dice, NUM_REGIONS);
   
   Game g = newGame(discipline, dice);
-  while (getKPIpoints(Game g, int player) < WIN_KPI) {
-    // TODO: Add loop body
+  int currentPlayer = NO_ONE;
+  int diceValue;
+  while (getKPIpoints(g, currentPlayer) < WIN_KPI) {
+    diceValue = rollDice();
+    throwDice(g, diceValue);
+    currentPlayer = getWhoseTurn(g);
     
+    action move = getMove(g);
+    while (move.actionCode != PASS){
+      assert(isLegalAction(g, move));
+      if (move.actionCode == START_SPINOFF) {
+        diceValue = rollDice();
+        if (diceValue <= 4 || dicevalue >= 10) { // Gives probability of 1/3
+          move.actionCode = OBTAIN_IP_PATENT;
+        } else {
+          move.actionCode = OBTAIN_PUBLICATION;
+        }
+      }
+      makeAction(g, move);
+      move = getMove();
+    }
   }
 }
 
-// Reads in an array of numbers 
+// Reads in an array of numbers
+// into array[], of lenght num.
+// Numbers must be comma separated
+// with no whitespace.
 void getArray(int array[], int num) {
   int i = 0;
   while (i < num) {
@@ -48,4 +72,16 @@ void getArray(int array[], int num) {
     i++;
   }
   return;
+}
+
+int rollDice() {
+  int random;
+  scanf("%d", &random);
+  return random;
+}
+
+action getMove(Game g) {
+  action move;
+  // TODO: Write getMove
+  return move;
 }
