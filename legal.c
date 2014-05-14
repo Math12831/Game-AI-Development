9afa5012158270c4482138c295c93fc1b46129cf
Nicholas Pizzino <pizzaboy@iprimus.com.au>
2014-05-14 18:29:48 +0800

    Initial isLegal function.
    Not completely done, will try to work on when I have the time.

---
+++ legal.c
@@ -0,0 +1,77 @@
+//
+//  legal.c
+//  UNSW
+//
+//  Created by Nicholas Pizzino on 5/14/14.
+//  Copyright (c) 2014 Pachino's Casinos. All rights reserved.
+//
+
+#define NOBODY 0
+#define SOMEBODY 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Game.h"
+
+int validatePath (Game g, path pathToVertex);
+int checkResources (Game g, int actionCode);
+int checkNoNeighbours (Game g, path pathToVertex);
+int checkDisciplineNumbers (Game g);
+int isLegalAction (Game g, action a);
+
+int isLegalAction (Game g, action a) {
+    int tally = 0;
+    if (getWhoseTurn (Game g) != NO_ONE) {
+        if (a.actionCode == BUILD_CAMPUS) {
+            if (validatePath (g, a.destination) == FALSE && checkResources (g, a.actionCode) == FALSE) {
+                tally ++;
+            }
+            if (checkNeighbours (g, a.destination) != NOBODY && getCampus (g, a.destination) != VACANT_VERTEX) {
+                tally ++;
+            }
+            if (getARC (g, a.destination) != getWhoseTurn (g)) {
+                tally ++;
+            }
+        } else if (a.actionCode == BUILD_G08) {
+            if (validatePath (g, a.destination) == FALSE && checkResources (g, a.actionCode) == FALSE) {
+                tally ++;
+            }
+            if (getCampus (g, a.destination) != getWhoseTurn (g)) {
+                tally ++;
+            }
+            int numberOfG08s = getGO8s (g, UNI_A);
+            numberOfG08s += getGO8s (g, UNI_B);
+            numberOfG08s += getGO8s (g, UNI_C);
+            if (numberOfG08s >= 8) {
+                tally ++;
+            }
+        } else if (a.actionCode == OBTAIN_ARC) {
+            if (validatePath (g, a.destination) == FALSE && checkResources (g, a.actionCode) == FALSE) {
+                tally ++;
+            }
+            if (getARC (g, a.destination) != VACANT_ARC) {
+                tally ++;
+            }
+        } else if (a.actionCode == START_SPINOFF) {
+            if (checkResources (g, a.actionCode) == FALSE) {
+                tally ++;
+            }
+        } else if (a.actionCode == RETRAIN_STUDENTS) {
+            int exchangeRate = getExchangeRate (g, getWhoseTurn (g), a.disciplineFrom, a.disciplineTo);
+            if (exchangeRate != 3) {
+                tally ++;
+            }
+            if (checkResources (g, a.actionCode) != TRUE) {
+                tally ++;
+            }
+            if (a.disciplineFrom == STUDENT_THD) {
+                tally ++;
+            }
+            
+        }
+    }
+    if (tally > 0) {
+        valid = FALSE;
+    }
+    return valid;
+}
